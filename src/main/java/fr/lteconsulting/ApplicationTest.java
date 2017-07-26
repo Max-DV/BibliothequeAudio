545fdd6c01a9a5f13707984ce2026c2202c5a594
Arnaud Tournier <ltearno@gmail.com>
2017-07-26 16:38:07 +0200

    debut dao disques

--- src/main/java/fr/lteconsulting/ApplicationTest.java
+++ src/main/java/fr/lteconsulting/ApplicationTest.java
@@ -1,5 +1,7 @@
 package fr.lteconsulting;
 
+import java.util.UUID;
+
 import fr.lteconsulting.dao.DisqueDAO;
 import fr.lteconsulting.modele.Disque;
 
@@ -11,6 +13,21 @@ public class ApplicationTest
 
 		chercherEtAfficherDisque( dao, "pptt" );
 		chercherEtAfficherDisque( dao, "ppttdddd" );
+
+		System.out.println( "AJOUT DE DISQUES" );
+		for( int i = 0; i < 10; i++ )
+		{
+			Disque ajout = dao.add( new Disque( "Toto" ) );
+			ajout.afficher();
+		}
+
+		Disque disque = dao.findById( "pptt" );
+		disque.setNom( "DISQUE " + UUID.randomUUID().toString() );
+		dao.update( disque );
+
+		System.out.println( "BIBLIOTHEQUE COMPLETE" );
+		for( Disque d : dao.findAll() )
+			d.afficher();
 	}
 
 	private static void chercherEtAfficherDisque( DisqueDAO dao, String id )
--- src/main/java/fr/lteconsulting/dao/DisqueDAO.java
+++ src/main/java/fr/lteconsulting/dao/DisqueDAO.java
@@ -5,7 +5,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import fr.lteconsulting.modele.Disque;
 
@@ -58,19 +61,92 @@ public class DisqueDAO
 
 	public List<Disque> findAll()
 	{
-		return null;
+		try
+		{
+			List<Disque> disques = new ArrayList<>();
+
+			String sql = "SELECT * FROM `disques`";
+			PreparedStatement statement = connection.prepareStatement( sql );
+			ResultSet resultSet = statement.executeQuery();
+			while( resultSet.next() )
+			{
+				String id = resultSet.getString( "id" );
+				String nom = resultSet.getString( "nom" );
+
+				Disque disque = new Disque( id, nom );
+
+				disques.add( disque );
+			}
+
+			return disques;
+		}
+		catch( SQLException e )
+		{
+			throw new RuntimeException( "Impossible de réaliser l(es) opération(s)", e );
+		}
 	}
 
 	public Disque add( Disque disque )
 	{
-		return disque;
+		try
+		{
+			// génération d'un identifiant aléatoire, unique dans l'univers
+			String id = UUID.randomUUID().toString();
+
+			String sqlQuery = "INSERT INTO disques (`id`, `nom`) VALUES (?, ?)";
+
+			PreparedStatement statement = connection.prepareStatement( sqlQuery );
+			statement.setString( 1, id );
+			statement.setString( 2, disque.getNom() );
+
+			int nbEnregistrementInseres = statement.executeUpdate();
+			if( nbEnregistrementInseres == 0 )
+				throw new RuntimeException( "Aucun disque inséré" );
+
+			// met à jour l'objet disque avec l'id généré
+			// pour mettre l'appelant au courant de l'id généré
+			disque.setCodeBarre( id );
+
+			return disque;
+		}
+		catch( SQLException e )
+		{
+			throw new RuntimeException( "Impossible d'ajouter le disque", e );
+		}
 	}
 
 	public void update( Disque disque )
 	{
+		try
+		{
+			String sqlQuery = "UPDATE disques SET `nom` = ? WHERE id = ?";
+
+			PreparedStatement statement = connection.prepareStatement( sqlQuery );
+			statement.setString( 1, disque.getNom() );
+			statement.setString( 2, disque.getCodeBarre() );
+
+			statement.executeUpdate();
+		}
+		catch( SQLException e )
+		{
+			throw new RuntimeException( "Impossible de mettre à jour le disque", e );
+		}
 	}
 
 	public void delete( String id )
 	{
+		try
+		{
+			String sqlQuery = "DELETE FROM disques WHERE id = ?";
+
+			PreparedStatement statement = connection.prepareStatement( sqlQuery );
+			statement.setString( 1, id );
+
+			statement.executeUpdate();
+		}
+		catch( SQLException e )
+		{
+			throw new RuntimeException( "Impossible de retirer le disque", e );
+		}
 	}
 }
